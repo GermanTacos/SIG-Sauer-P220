329ee1ab09388a025c4a18f27fd142d749b779da
GermanTacos <jakebuilds@gmail.com>
2019-01-23 05:01:33 -0500

    various small tweaks

--- hooks/weaponfactorytweakdata.lua
+++ hooks/weaponfactorytweakdata.lua
@@ -1,23 +1,23 @@
 Hooks:PostHook( WeaponFactoryTweakData, "init", "P220ModInit", function(self)
 	
 	self.wpn_fps_pis_noodle.adds.wpn_fps_upg_fl_pis_laser = {
-			"wpn_fps_pis_beretta_body_rail"
+			"wpn_fps_pis_legacy_fl_mount"
 		}
 	self.wpn_fps_pis_noodle.adds.wpn_fps_upg_fl_pis_tlr1 = {
-			"wpn_fps_pis_beretta_body_rail"
+			"wpn_fps_pis_legacy_fl_mount"
 		}
 	self.wpn_fps_pis_noodle.adds.wpn_fps_upg_fl_pis_crimson = {
-			"wpn_fps_pis_beretta_body_rail"
+			"wpn_fps_pis_legacy_fl_mount"
 		}
 	self.wpn_fps_pis_noodle.adds.wpn_fps_upg_fl_pis_x400v = {
-			"wpn_fps_pis_beretta_body_rail"
+			"wpn_fps_pis_legacy_fl_mount"
 		}
 	self.wpn_fps_pis_noodle.adds.wpn_fps_upg_fl_pis_m3x = {
-			"wpn_fps_pis_beretta_body_rail"
+			"wpn_fps_pis_legacy_fl_mount"
 		}
 		
 	self.wpn_fps_pis_noodle.adds.wpn_fps_pis_noodle_fl_zom = {
-			"wpn_fps_pis_beretta_body_rail"
+			"wpn_fps_pis_legacy_fl_mount"
 		}
 		
 	for _, part_id in pairs(self.wpn_fps_pis_noodle.uses_parts) do
