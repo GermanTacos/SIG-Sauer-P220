e8af4070fdc8c1ca52948d7a3e933deedb2b91f9
GermanTacos <jakebuilds@gmail.com>
2019-01-26 00:32:17 -0500

    removed x400

--- hooks/weaponfactorytweakdata.lua
+++ hooks/weaponfactorytweakdata.lua
@@ -16,10 +16,6 @@ Hooks:PostHook( WeaponFactoryTweakData, "init", "P220ModInit", function(self)
 			"wpn_fps_pis_legacy_fl_mount"
 		}
 		
-	self.wpn_fps_pis_noodle.adds.wpn_fps_pis_noodle_fl_zom = {
-			"wpn_fps_pis_legacy_fl_mount"
-		}
-		
 	for _, part_id in pairs(self.wpn_fps_pis_noodle.uses_parts) do
 		if self.parts[part_id].type == "barrel_ext" then
 			self.parts.wpn_fps_pis_noodle_sl_zom.override[part_id] = {a_obj="a_ns_zmb"}
